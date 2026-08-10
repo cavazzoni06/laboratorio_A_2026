535a949310ff928d12b91e4a55e9bffa0e341604
cavazzoni06 <cavazzoni.stella@gmail.com>
2026-08-10 11:32:24 +0200

    aggiunta classi

--- src/CineMax.java
+++ src/CineMax.java
@@ -1,5 +1,12 @@
 public class CineMax{
 	public static void main(String[]args){
-	
+		System.out.println("Benvenuto! Cosa vuoi fare?");
+
+		System.out.println("1. Login");
+		System.out.println("2. Registrazione");
+		System.out.println("3. Procedi come guest");
+		System.out.println("4. Esci");
+		System.out.print("Inserisci la tua scelta: ");
+		
 	}
 }
---
+++ src/Proiezione.java
@@ -0,0 +1,5 @@
+public class Proiezione{
+	public static void main(String[]args){
+        
+	}
+}
