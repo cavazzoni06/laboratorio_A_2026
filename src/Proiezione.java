ec89e89a3bac5d16361e192b6d51c021514bae00
mkboubaker <b.maykamar@gmail.com>
2026-08-10 12:08:01 +0200

    Update Proiezione.java

--- src/Proiezione.java
+++ src/Proiezione.java
@@ -1,5 +1,23 @@
+import java.time.LocalDateTime;
 public class Proiezione{
-	public static void main(String[]args){
-        
+	private LocalDateTime dataOra;
+	private String titolo;
+	private String genre;
+	private String regista;
+	private int anno;
+	private int durateMinuti;
+	private int etaMinima;
+    private double prezzoBiglietto;
+
+	public Proiezione(LocalDateTime dataOra, String titolo, String genere; String rigista, int anno, int durateMinuti, int etaMinima, double prezzoBiglietto) {
+		this.DataOra = dataOra;
+		this.titolo = titolo;
+		this.genere = genere;
+		this.rigista = rigista;
+		this.anno = anno;
+		this.durateMinuti = durataMinuti;
+		this.etaMinima = etaMinima;
+		this.prezzoBiglietto = prezzoBiglietto;
 	}
-}
+	
+}	
