7c1d255d4054f6c12e60e2f026527b32945d28c6
mkboubaker <b.maykamar@gmail.com>
2026-08-18 12:05:19 +0200

    Update GestoreProiezioni.java

--- src/GestoreProiezioni.java
+++ src/GestoreProiezioni.java
@@ -1,84 +1,172 @@
-pachkage cinemax;
+package cinemax;
 
-
-import java.util.ArrayLits;
-import java.util.Lits;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.format.dateTimeFormatter
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+public class GestoreProiezioni {
 
-public class GestoreProiezioni  {
-  private Lits<Proiezione> proiezioni;
+    private List<Proiezione> proiezioni;
 
-  public GestoreProiezione() {
-    proiezioni = new ArrayLits();
-  }
-  public void caricaProiezioniDaFile(String percorso) Throws IOException {
-     DateTimeFormatter formatter =
-             DateTimeFormatter.ofPattern("yyy-MM-dd HH:ss");
-     try (BufferedReader reader = new BufferredReader(new FileReader(percorso))){
-         String riga;
-         // Salta La prima riga del file csv
-       reader.readLine();
-       while ((riga = reader.readerLine() != null){
-             String[] dati = riga.split(",");
-              LocalDateTime dataOra =
-                       LocaleDateTime.parse(dati[0].replace("\"",""), formatter);
-              String titolo = dati[1].replace("\"","");
-              String genere = dati[2].replace("\"","");
-              String regista = dati[3].replace("\"","");
+    public GestoreProiezioni() {
+        proiezioni = new ArrayList<>();
+    }
 
-              int anno = Integer.parseInt(dati[4]);
-              int durataMinuti = Integer.parseInt(dati[5]);
-              int etaMinima = Integer.parseInt(dati[6]);
-              double prezzoBiglietto = double.parseDouble(dati[7]);
-         
-              Proiezione proiezione = new proiezione(
-                      dataOra,
-                      titolo,
-                      genere,
-                      regista,
-                      anno,
-                      durataMinuti,
-                      etaMinima,
-                      prezzoBiglietto
-              );
-              proiezioni.add(proiezione);
-              
-       }
-       
-     }
-  }
-  public Lista<Proiezione> getProiezioni(){
-       return proiezione;
-  }
-  public List<proiezione> cercaProiezione(String titolo) {
-     Lista<Proiezione> risulatti = new ArrayList<>();
-    for (Proiezione p : proiezione) {
-        if (p.getTitolo().toLowerCase().contains(titolo.toLowerCase())){
-            risultati.add(p);
+    public void caricaProiezioniDaFile(String percorso) throws IOException {
+
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+        try (BufferedReader reader =
+                     new BufferedReader(new FileReader(percorso))) {
+
+            String riga;
+
+            // Salta la prima riga del file CSV
+            reader.readLine();
+
+            while ((riga = reader.readLine()) != null) {
+
+                String[] dati = riga.split(",");
+
+                LocalDateTime dataOra =
+                        LocalDateTime.parse(
+                                dati[0].replace("\"", ""),
+                                formatter
+                        );
+
+                String titolo = dati[1].replace("\"", "");
+                String genere = dati[2].replace("\"", "");
+                String regista = dati[3].replace("\"", "");
+
+                int anno = Integer.parseInt(dati[4]);
+                int durataMinuti = Integer.parseInt(dati[5]);
+                int etaMinima = Integer.parseInt(dati[6]);
+                double prezzoBiglietto =
+                        Double.parseDouble(dati[7]);
+
+                Proiezione proiezione = new Proiezione(
+                        dataOra,
+                        titolo,
+                        genere,
+                        regista,
+                        anno,
+                        durataMinuti,
+                        etaMinima,
+                        prezzoBiglietto
+                );
+
+                proiezioni.add(proiezione);
+            }
         }
     }
-    return risultati;
-  }
-  public boolean aggiungiproiezione(Proiezione nuovaProiezione) {
-      LocalDateTime nuovaInizio = nuovaProiezione.getDataOra();
-      LocalDateTime nuovaFine = nuovaInizio.plusMinutes(
-             nuovaProiezione.getDurateMinuti()
-      );
-      for (Proiezione p : proiezione){
-          LocalDateTime inizio = p.getDataOra();
-          LocaleDateTime fine = inizio.plusMinutes(
-                 p.getDurataMinuti()
-          );
-        if (nuovaInizio.isBefore(fine) && nuovaFine.isAfter(inizio)) {
-           return false;
+
+    public List<Proiezione> getProiezioni() {
+        return proiezioni;
+    }
+
+    public List<Proiezione> cercaProiezione(String titolo) {
+
+        List<Proiezione> risultati = new ArrayList<>();
+
+        for (Proiezione p : proiezioni) {
+
+            if (p.getTitolo().toLowerCase()
+                    .contains(titolo.toLowerCase())) {
+
+                risultati.add(p);
+            }
         }
-      }
-    proiezione.add(nuovaProiezione);
-    return true;
-  }
-  
+
+        return risultati;
+    }
+
+    public void visualizzaProiezione(Proiezione p) {
+
+        System.out.println("Titolo: " + p.getTitolo());
+        System.out.println("Data/Ora: " + p.getDataOra());
+        System.out.println("Costo: " + p.getPrezzoBiglietto() + " €");
+        System.out.println("Posti liberi: 200");
+    }
+
+    public boolean aggiungiProiezione(
+            Proiezione nuovaProiezione) {
+
+        LocalDateTime nuovaInizio =
+                nuovaProiezione.getDataOra();
+
+        LocalDateTime nuovaFine =
+                nuovaInizio.plusMinutes(
+                        nuovaProiezione.getDurataMinuti()
+                );
+
+        for (Proiezione p : proiezioni) {
+
+            LocalDateTime inizio = p.getDataOra();
+
+            LocalDateTime fine =
+                    inizio.plusMinutes(
+                            p.getDurataMinuti()
+                    );
+
+            if (nuovaInizio.isBefore(fine)
+                    && nuovaFine.isAfter(inizio)) {
+
+                return false;
+            }
+        }
+
+        proiezioni.add(nuovaProiezione);
+
+        return true;
+    }
+
+    public boolean modificaProiezione(
+            Proiezione proiezione,
+            Proiezione nuovaProiezione) {
+
+        proiezione.setDataOra(
+                nuovaProiezione.getDataOra()
+        );
+
+        proiezione.setTitolo(
+                nuovaProiezione.getTitolo()
+        );
+
+        proiezione.setGenere(
+                nuovaProiezione.getGenere()
+        );
+
+        proiezione.setRegista(
+                nuovaProiezione.getRegista()
+        );
+
+        proiezione.setAnno(
+                nuovaProiezione.getAnno()
+        );
+
+        proiezione.setDurataMinuti(
+                nuovaProiezione.getDurataMinuti()
+        );
+
+        proiezione.setEtaMinima(
+                nuovaProiezione.getEtaMinima()
+        );
+
+        proiezione.setPrezzoBiglietto(
+                nuovaProiezione.getPrezzoBiglietto()
+        );
+
+        return true;
+    }
+
+    public boolean eliminaProiezione(
+            Proiezione proiezione) {
+
+        return proiezioni.remove(proiezione);
+    }
 }
