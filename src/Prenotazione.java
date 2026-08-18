cfe8a5df6130b03e9ffa8eefe6d8ac29040d7aba
tunsimf00 <aziz.haddajipriv@gmail.com>
2026-08-18 12:43:26 +0200

    Refactor Prenotazione class with new methods

--- src/Prenotazione.java
+++ src/Prenotazione.java
@@ -7,13 +7,21 @@ package cinemax;
  * e contiene il cliente, la proiezione scelta e il numero
  * di biglietti prenotati.
  *
- * @author Nome Cognome
+ * Sede di Varese (VA)
+ 766768 Cavazzoni Stella
+ 760763 Haddaji Aziz
+ 755773 Boubaker May Kamar
+ 763711 Marzouki Siwar
  */
 public class Prenotazione {
 
+    /** Codice univoco della prenotazione. */
     private String codice;
+    /** Username del cliente che ha effettuato la prenotazione. */
     private String usernameCliente;
+    /** Proiezione associata alla prenotazione. */
     private Proiezione proiezione;
+    /** Numero di biglietti prenotati. */
     private int numeroBiglietti;
 /**
  * Crea una nuova prenotazione.
@@ -22,6 +30,8 @@ public class Prenotazione {
  * @param usernameCliente username del cliente che effettua la prenotazione
  * @param proiezione proiezione scelta dal cliente
  * @param numeroBiglietti numero di biglietti prenotati
+ * @throws IllegalArgumentException se il numero di biglietti
+ *         è minore o uguale a zero
  */
     public Prenotazione(String codice,String usernameCliente,Proiezione proiezione,int numeroBiglietti){
         if (numeroBiglietti <= 0) {
@@ -64,11 +74,34 @@ public class Prenotazione {
     public int getNumeroBiglietti() {
         return numeroBiglietti;
     }
-    public double getCostoTotale() {
-        return numeroBiglietti * proiezione.getPrezzoBiglietto();
-    }
-    @Override
-    public String toString() {
-        return "Codice prenotazione: " + codice + "\nCliente: " + usernameCliente + "\nProiezione: " + proiezione + "\nNumero biglietti: " + numeroBiglietti + "\nCosto totale: " + getCostoTotale() + " €";
-    }
+/**
+ * Calcola il costo totale della prenotazione.
+ *
+ * @return costo totale della prenotazione
+ */
+public double getCostoTotale() {
+    return numeroBiglietti * proiezione.getPrezzoBiglietto();
+}
+
+/**
+ * Restituisce una rappresentazione testuale della prenotazione.
+ *
+ * @return stringa contenente i dati della prenotazione
+ */
+@Override
+public String toString() {
+    return "Codice prenotazione: " + codice
+            + "\nCliente: " + usernameCliente
+            + "\nProiezione: " + proiezione
+            + "\nNumero biglietti: " + numeroBiglietti
+            + "\nCosto totale: " + getCostoTotale() + " €";
+}
+
+/**
+ * Modifica la proiezione associata alla prenotazione.
+ *
+ * @param proiezione nuova proiezione
+ */
+public void setProiezione(Proiezione proiezione) {
+    this.proiezione = proiezione;
 }
