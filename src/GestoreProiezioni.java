c1965c63fa3baa3649ae6e2f90619f604241a86c
mkboubaker <b.maykamar@gmail.com>
2026-08-18 10:46:26 +0200

    Update GestoreProiezioni.java

--- src/GestoreProiezioni.java
+++ src/GestoreProiezioni.java
@@ -1,1 +1,12 @@
+pachkage cinemax;
 
+import java.util.ArrayLits;
+import java.util.Lits;
+
+public class GestoreProiezioni  {
+  private Lits<Proiezione> proiezioni;
+
+  public GestoreProiezione() {
+    proiezioni = new ArrayLits();
+  }
+}
